9d2706fbb9b886cd65bb796369783a21aab029e5
AndrusenkoSerhii <andrusenkoserhiy@gmail.com>
2024-08-11 21:58:48 +0300

    Setup tiles and character controller - draft

--- Assets/Scripts/World/ChunkController.cs
+++ Assets/Scripts/World/ChunkController.cs
@@ -16,20 +16,22 @@ namespace World{
       for (int k = 0; k < 3; k++){
         for (int n = 0; n < 3; n++){
           var startChunk = _chunkGenerator.GetChunk(k, n);
-          float stepX = 16f;
-          float stepY = 16f;
+          float stepX = 1.32f;
+          float stepY = 1.3f;
           Vector3 coords = Vector3.zero;
-          coords.x = k * 1600f;
+          coords.x = k * 1320f;
+          coords.y = n * 1300f;
           for (int i = 0; i < startChunk.width; i++){
             for (int j = 0; j < startChunk.height; j++){
               if (startChunk.GetCellData(i, j).perlin > 0.4f){
                 var cellObject = _cellObjectsPool.GetObject();
                 cellObject.transform.position = coords;
+                cellObject.name = i + " " + j + " ("+k+" "+n+")";
               }
 
               coords.x += stepX;
             }
-            coords.x = k * 1600f;
+            coords.x = k * 1320f;
             coords.y -= stepY;
           }
         }

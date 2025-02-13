b2897f6c024b1aefd1c592e9e5bfde8cfc0a84e0
Oleh Mukoyed <oleh.mukoyed@gmail.com>
2025-02-13 11:47:25 +0200

    add service locator

---
+++ Assets/Scripts/SeviceLocator/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UnityServiceLocator {
+  [DisallowMultipleComponent]
+  [RequireComponent(typeof(ServiceLocator))]
+  public abstract class Bootstrapper : MonoBehaviour {
+    private ServiceLocator container;
+    protected internal ServiceLocator Container => container.OrNull() ?? (container = GetComponent<ServiceLocator>());
+
+    private bool hasBeenBootstrapped;
+
+    public void Awake() => BootstrapOnDemand();
+
+    public void BootstrapOnDemand() {
+      if (hasBeenBootstrapped) return;
+      hasBeenBootstrapped = true;
+      Bootstrap();
+    }
+
+    protected abstract void Bootstrap();
+  }
+}

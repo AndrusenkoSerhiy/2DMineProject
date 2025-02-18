80726ffd788e9c1f919739d688a372cee85b20b6
Oleh Mukoyed <oleh.mukoyed@gmail.com>
2025-02-18 11:39:45 +0200

    add scriptable base class with guid

---
+++ Assets/Scripts/Editor/GuidPropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using Scriptables;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BaseScriptableObject), true)]
+public class BaseScriptableObjectEditor : Editor {
+  public override void OnInspectorGUI() {
+    var scriptable = (BaseScriptableObject)target;
+
+    EditorGUILayout.LabelField("Id", scriptable.Id, EditorStyles.textField);
+
+    if (GUILayout.Button("Regenerate Id")) {
+      Undo.RecordObject(scriptable, "Regenerate Id");
+      scriptable.RegenerateId();
+      EditorUtility.SetDirty(scriptable);
+    }
+
+    DrawDefaultInspector();
+  }
+}
---
+++ Assets/Scripts/Scriptables/BaseScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scriptables {
+  public abstract class BaseScriptableObject : ScriptableObject {
+    [SerializeField, HideInInspector] private string id = Guid.NewGuid().ToString();
+    public string Id => id;
+
+    public void RegenerateId() {
+      id = Guid.NewGuid().ToString();
+    }
+
+    private void OnValidate() {
+      if (string.IsNullOrEmpty(id)) {
+        id = Guid.NewGuid().ToString();
+      }
+    }
+  }
+}

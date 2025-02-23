bda6506b90bec8d17c3e0d6300c9bf1e1168a748
AndrusenkoSerhiy <andrusenkoserhiy@gmail.com>
2025-02-23 12:48:43 +0200

    Recipes and datas

--- Assets/Scripts/Scriptables/Craft/RecipeType.cs
+++ Assets/Scripts/Scriptables/Craft/RecipeType.cs
@@ -3,7 +3,11 @@ using System;
 namespace Scriptables.Craft {
   [Serializable]
   public enum RecipeType {
-    Workbench,
-    Forge
+    Handcraft = 0,
+    Stonecutter = 1,
+    Workbench = 2,
+    Forge = 3,
+    Foodstation = 4,
+    ChemicalStation = 5
   }
 }

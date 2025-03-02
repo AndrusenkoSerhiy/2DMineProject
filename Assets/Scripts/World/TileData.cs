bbe9daec2fa86d199ffc4f47dfce4c1f4db89ba5
AndrusenkoSerhiy <andrusenkoserhiy@gmail.com>
2025-03-02 13:17:05 +0200

    ColliderParams from ResourceData

--- Assets/Scripts/World/TileData.cs
+++ Assets/Scripts/World/TileData.cs
@@ -6,12 +6,7 @@ namespace World {
   public class TileData {
     [SerializeField] private Sprite sprite;
     [SerializeField] private int offsetSorting;
-    [SerializeField] private Vector3 offsetPosition;
-    [SerializeField] private Vector3 offsetCollider;
-
     public Sprite Sprite => sprite;
     public int OffsetSorting => offsetSorting;
-    public Vector3 OffsetPosition => offsetPosition;
-    public Vector3 OffsetCollider => offsetCollider;
   }
 }
